cbf2638ade963770af350862d51e87763d67d94d
YuanLinDUT <yuanlin_postbox@163.com>
2020-05-03 12:54:29 +0800

    removed unnecessary files

--- .ipynb_checkpoints/train-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import sys
-from configs.train_args import args
-import trains
-from PIL import ImageFile
-ImageFile.LOAD_TRUNCATED_IMAGES = True
-
-if sys.argv[1]=="retinanet":
-    trains.train_retinanet()
